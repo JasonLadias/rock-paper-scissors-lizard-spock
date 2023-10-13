72bb93171aa4036edc1225885b7da60363e247e8
JasonLadias <jason@everywhere.io>
2023-10-13 16:25:28 +0300

    Remove unused function

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,14 +1,3 @@
-import { ethers } from 'ethers';
-
-export const hash = (move: number, salt: Uint8Array) => {
-    // Combine the values into a single array
-    const combined = ethers.defaultAbiCoder.encode(['uint8', 'uint256'], [move, salt]);
-
-    // Compute the keccak256 hash
-    return ethers.keccak256(combined);
-}
-
-
 
 type result = boolean | "Draw";
 
