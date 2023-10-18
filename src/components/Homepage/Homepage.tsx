618ca8157770972a2d56a0b4a24e05c754279c63
JasonLadias <jason@everywhere.io>
2023-10-18 18:15:14 +0300

    Add Fancier HomePage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,5 +1,11 @@
-import { Container, Button, Box, TextField, Typography } from "@mui/material";
-import { grey, yellow } from "@mui/material/colors";
+import {
+  Container,
+  Button,
+  Box,
+  TextField,
+  Typography,
+  Divider,
+} from "@mui/material";
 import { FC, useState } from "react";
 import { useRouter } from "next/router";
 import Anchor from "../Anchor";
@@ -36,102 +42,117 @@ const Homepage: FC = () => {
   };
 
   return (
-    <Container
-      maxWidth="sm"
+    <Box
       sx={{
-        minHeight: "100vh",
-        py: 5,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        gap: 5,
-        alignItems: "center",
+        height: "100vh",
+        backgroundImage: `url(background.jpg), linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.6))`,
+        backgroundSize: "cover",
+        backgroundBlendMode: "darken",
+        py: { xs: 4, md: 10 },
+        px: 2,
       }}
     >
-      <Head>
-        <title key="title">Rock Paper Scissors Spock Lizard</title>
-      </Head>
-      <Typography
-        variant="h4"
-        fontWeight="bold"
-        textAlign="center"
-        color="primary"
-      >
-        Home Page
-      </Typography>
-      <Box
+      <Container
+        maxWidth="sm"
         sx={{
-          p: 3,
-          bgcolor: yellow[500],
-          borderRadius: 2,
-          boxShadow: 3,
           display: "flex",
           flexDirection: "column",
-          gap: 3,
-          width: "100%",
+          alignItems: "flex-end",
+          justifyContent: "space-between",
+          height: "80vh",
+          bgcolor: "transparent",
         }}
       >
-        <Typography
-          variant="body1"
-          fontWeight="bold"
-          textAlign="left"
-          color="primary"
+        <Head>
+          <title key="title">Rock Paper Scissors Spock Lizard</title>
+        </Head>
+        <Box
+          sx={{
+            width: "100%",
+          }}
         >
-          Note: This game should be played on GOERLI TESTNET
-        </Typography>
-      </Box>
+          <Typography
+            color="white"
+            align="center"
+            variant="h3"
+            sx={{
+              textShadow: "3px 3px 3px rgba(0,0,0, 0.5)",
+            }}
+          >
+            Rock Paper Scissors Lizard Spock
+          </Typography>
+        </Box>
 
-      <Anchor href="/create-game">
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          sx={{ width: "100%", fontSize: "1.2rem" }}
+        <Box
+          sx={{
+            p: 3,
+            width: "100%",
+            borderRadius: "15px",
+            bgcolor: "white",
+            boxShadow: "8px 8px 16px #d1d9e6, -8px -8px 16px #ffffff",
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
         >
-          Start New Game
-        </Button>
-      </Anchor>
-      <Box
-        sx={{
-          p: 3,
-          bgcolor: grey[200],
-          borderRadius: 2,
-          boxShadow: 3,
-          display: "flex",
-          flexDirection: "column",
-          gap: 3,
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
-        <Typography variant="subtitle1" textAlign="center" fontWeight="medium">
-          OR ENTER PREVIOUS GAME <br /> SMART CONTRACT
-        </Typography>
-
-        <TextField
-          id="outlined-basic"
-          label="Enter Smart Contract"
-          variant="outlined"
-          fullWidth
-          value={existingContract}
-          onChange={handleExistingContract}
-          sx={{ "& .MuiOutlinedInput-root": { borderRadius: 2 } }}
-          error={!!existingContractError}
-          helperText={existingContractError}
-        />
-
-        <Button
-          variant="contained"
-          color="secondary"
-          size="large"
-          disabled={!existingContract}
-          onClick={handleSubmit}
-          sx={{ width: "100%", fontSize: "1.2rem" }}
-        >
-          Join Existing Game
-        </Button>
-      </Box>
-    </Container>
+          <Anchor href="/create-game">
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              sx={{
+                width: "100%",
+                fontSize: "1.1rem",
+                borderRadius: "12px",
+                //boxShadow: "4px 4px 10px #d1d9e6, -4px -4px 10px #ffffff",
+                "&:hover": {
+                  boxShadow: "2px 2px 5px #d1d9e6, -2px -2px 5px #ffffff",
+                },
+              }}
+            >
+              Start New Game
+            </Button>
+          </Anchor>
+          <Divider />
+          <Typography variant="h6" textAlign="center" fontWeight={500}>
+            Or enter an existing Smart Contract
+          </Typography>
+          <Box sx={{ p: 1, bgcolor: "white" }}>
+            <TextField
+              id="outlined-basic"
+              label="Enter Smart Contract"
+              variant="outlined"
+              fullWidth
+              value={existingContract}
+              onChange={handleExistingContract}
+              sx={{
+                "& .MuiOutlinedInput-root": { borderRadius: "12px" },
+                "& .MuiInputLabel-root": { fontWeight: 500 },
+              }}
+              error={!!existingContractError}
+              helperText={existingContractError}
+            />
+          </Box>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            disabled={!existingContract}
+            onClick={handleSubmit}
+            sx={{
+              width: "100%",
+              fontSize: "1.1rem",
+              borderRadius: "12px",
+              "&:hover": {
+                boxShadow: "2px 2px 5px #d1d9e6, -2px -2px 5px #ffffff",
+              },
+            }}
+          >
+            Join Existing Game
+          </Button>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
